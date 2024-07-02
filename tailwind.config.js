c4107f91fbc761d7d18e0ae8a4527a29ccd36240
BENAHMED Djilali <djilalibenahmedd@gmail.com>
2024-07-02 21:42:43 +0100

    added styles, Colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Anzac: {
+          50: '#fcf9ea',
+          100: '#fcf9ea',
+          200: '#f3e195',
+          300: '#ebca59',
+          400: '#E6B93D',
+          500: '#d49c1e',
+          600: '#b77917',
+          700: '#b77917',
+          800: '#b77917',
+          900: '#683a1b',
+          950: '#683a1b',
+        },
+      }
+    },
   },
   plugins: [],
 }
